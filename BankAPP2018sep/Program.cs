15c56dd622e3e75f6a47c57c56b7e097cdbde680
Swathi Chakkilam <swathichakkilam@gmail.com>
2018-11-02 00:13:39 -0700

    Implemented my first design pattern class named VirtualBank and created shared memory space for VirtualBank Class

--- BankAPP2018sep/Program.cs
+++ BankAPP2018sep/Program.cs
@@ -7,29 +7,24 @@ namespace BankAPP2018sep
         static void Main(string[] args)
         {
             //account is the reference in memory to the Account object that has been created 
-            var account1 = new Account()
-            {
-              
-                AccountType = "Savings",
-                EmailAddress = "test@test.com"
-            };
-            account1.Deposit(10000.50M);
+            var account1 = VirtualBank.CreateAccount("user1@bank.com", "Savings",0);
+            account1.Deposit(100000.50M);
             Console.WriteLine($"AN:{account1.AccountNumber},AccType:{account1.AccountType},Balance:{account1.Balance},EA:{account1.EmailAddress}");
 
 
-            var account2 = new Account()
-            {
+            var account2 = VirtualBank.CreateAccount("user2@bank.com", "Checking", 9000M);
+           /* {
                // AccountNumber = 229,
                 AccountType = "Checking",
                 //Balance = 9000,
                 EmailAddress = "user2@bank.com"
-            };
-            account2.Deposit(2000.54M);
+            };*/
+           // account2.Deposit(2000.54M);
             account2.Withdraw(555M);
             Console.WriteLine($"AN:{account2.AccountNumber},AccType:{account2.AccountType},Balance:{account2.Balance},EA:{account2.EmailAddress}");
 
 
-            Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ BankAPP2018sep/VirtualBank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAPP2018sep
+{
+    /// <summary>
+    /// The VirtualBank Class is Static meaning only one instance of the class is created 
+    /// which is one shared memory among 
+    /// various customers holding an account with the bank
+    /// </summary>
+    /*when the class is declared static 
+     * no method in that class can be instantiated hence every method (member) in that class
+     * needs to be static */
+    static class VirtualBank
+    {
+        #region Methods 
+        /// <summary>
+        /// Method to Create an Account with the Bank
+        /// </summary>
+        /// <param name="EmailID"> Email Address of the User</param>
+        /// <param name="Accounttype">What type of Account User wants to create at the Bank</param>
+        /// <param name="InitialDeposit">The Amount Customer wishes to Deposit with the Bank</param>
+        /// <returns>returns the newly created account to the user</returns>
+        /*The return type of CreateAccount method 
+         * is the class Account type  as the account will be created
+         with the bank*/
+         //since Virtual Bank is a static class CreateAccount method is also static
+        public static Account CreateAccount(string EmailID, string Accounttype, decimal InitialDeposit)
+        {
+            var account = new Account
+            {
+                EmailAddress = EmailID,
+                AccountType = Accounttype
+
+            };
+            /*since balance cannot be set by the customer and it has to be set via Bank
+             * we mention a condition that if initial amount deposited is more than zero bank deposits in
+             the respective Bank account created for the customer*/
+
+            if (InitialDeposit > 0)
+            {
+                account.Deposit(InitialDeposit);
+            }
+            return account;
+        }
+        #endregion
+    }
+}
